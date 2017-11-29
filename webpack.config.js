883636d68c0d45a771927047eb63973a0e5009ac
Marcus Guthrie <marcusguthrie111@gmail.com>
2017-11-29 11:09:50 +0000

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,53 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            // question mark allows for scss and css files
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-        }]
-    },
-    // source map - allows bugs in different modules to be located easily in browser dev tools
-    // can go on webpack.js.org to choose different types
-    devtool: 'cheap-module-eval-source-map',
-    // webpack has its own live server
-    devServer: {
-        // other options below on website, only need 1 in this case
-        contentBase: path.join(__dirname, 'public')
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
+    console.log('env', env)
+    return {
+            entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                // question mark allows for scss and css files
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        // source map - allows bugs in different modules to be located easily in browser dev tools
+        // can go on webpack.js.org to choose different types
+        devtool: isProduction ? 'source-map' : 'inline-source-map',
+        // webpack has its own live server
+        devServer: {
+            // other options below on website, only need 1 in this case
+            contentBase: path.join(__dirname, 'public')
+        }
     }
 };
